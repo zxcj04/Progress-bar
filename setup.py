d0ec2955993977f943bdb5fb1808ff8e9b0d2bac
FanRende <zxcj04@gmail.com>
2024-09-24 13:19:55 +0800

    refactor: Rename package to mortalprogressbar and update project URL
    The setup.py file was modified to rename the package from "aclprogressbar" to "mortalprogressbar". Additionally, the project URL was updated to "https://github.com/zxcj04/Progress-bar". This commit improves the clarity and consistency of the project.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="aclprogressbar",
+    name="mortalprogressbar",
     description="A package for simple progress bar",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/zxcj04/Progress-bar",
     author="FanRende Huang",
     package_dir={"": "src"},
     version="1.0.1",
